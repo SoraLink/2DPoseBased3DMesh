e52d9ca23ba93c345078876e51b963e5800f7395
Sora <qitianye1104@gmail.com>
2026-04-17 13:38:22 +0800

    fill hole

--- residual_mesh_cutter.py
+++ residual_mesh_cutter.py
@@ -172,7 +172,7 @@ class ResidualMeshCutter:
 
                 # 🎯 想要多鼓，就调这个参数！目前是向外顶出 4 厘米 (0.04)。如果觉得不够，改成 0.06 或 0.08
                 max_bulge = 0.04
-                displacement = np.outer(weights * max_bulge, c_normal)
+                displacement = np.outer(weights * max_bulge, -c_normal)
 
                 # 暴力干预：让这批顶点沿着法向量突围
                 sealed_mesh.vertices[mask] += displacement
